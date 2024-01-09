f8d9e5b61441045b160846e95d0e116620cfa6c2
FedericaSidoti <federicasidoti21@gmail.com>
2024-01-09 18:53:36 +0100

    ADD-select values in edit.blade.php

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -49,8 +49,9 @@ class ProjectController extends Controller
     public function edit(Project $project)
 
     {
+        $types = Type::all();
 
-        return view('admin.projects.edit', compact('project'));
+        return view('admin.projects.edit', compact('project', 'types'));
     }
 
     public function update(Request $request, Project $project)
@@ -60,7 +61,8 @@ class ProjectController extends Controller
         $request->validate([
             'title' => 'required|max:255|',
             'thumb' => 'required|url',
-            'description' => 'required'
+            'description' => 'required',
+            'type_id' => 'nullable|exists:types,id'
         ]);
 
         $project->update($data);
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -20,6 +20,14 @@
                 <label for="description" class="form-label">Descrizione</label>
                 <textarea class="form-control" name="description" id="description" rows="4" placeholder="Descrizione del progetto">{{old('description', $project->description)}}</textarea>
             </div>
+
+            <select name="type_id" class="form-control" id="type_id">
+                <option>Seleziona una categoria</option>
+                @foreach($types as $type)
+                    <option @selected( old('type_id', optional($project->type)->id) == $type->id ) value="{{ $type->id }}">{{ $type->name }}</option>
+                @endforeach
+            </select>
+
             <div>
                 <input type="submit" class="btn btn-primary" value="Modifica">
             </div>
